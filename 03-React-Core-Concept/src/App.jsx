ea8a2c67501e1a14551eb2a4e855afdd3b63abd7
Sujon Pal <dev.sujonpal@gmail.com>
2026-05-03 22:15:24 +0600

    Access form data using onSubmit and e target

---
+++ 03-React-Core-Concept/src/App.jsx
@@ -0,0 +1,14 @@
+import Form from "./component/Page/Form";
+import Nav from "./component/Page/Nav";
+
+
+const App = () => {
+  return (
+    <div>
+      <Nav></Nav>
+      <Form></Form>
+    </div>
+  );
+};
+
+export default App;
